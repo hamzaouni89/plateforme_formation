96e268e23b1ae344bc2e05925034631d0d0c5f2d
hamzainfo89 <hamzaouni@gmail.com>
2019-03-18 14:04:49 +0100

    function register candidat

---
+++ api/apiCours.js
@@ -0,0 +1,98 @@
+var express = require('express')
+var router = express.Router();
+var multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+
+});
+
+const upload = multer({
+    storage: storage,
+});
+
+const Cours = require('../model/cours')
+
+router.post('/upload', upload.single("Image"), function (req, res, next) {
+})
+router.get('/getImage/:name', function (req, res, next) {
+    res.sendFile('../uploads/' + req.params.name);
+})
+router.post('/addCours', function (req, res, next) {
+    console.log(req.body);
+    var cours = new Cours({
+        date: new Date(),
+        titre: req.body.titre,
+        contenue: req.body.contenue,
+        image: req.body.image,
+        type: req.body.type,
+        owner: req.body.owner,
+        number: req.body.number
+
+
+
+    });
+    console.log(req.body)
+    cours.save(function (err, cours) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(cours)
+        }
+    })
+})
+router.get('/getCours', function (req, res, next) {
+    Cours.find().populate('owner').exec(function (err, cours) {
+        if (err) {
+            res.send(err)
+        }
+        else {
+            res.send(cours)
+        }
+    })
+})
+
+router.get('/getCours/:id',  function (req, res, next) {
+    var id = req.params.id
+    Cours.findById(id).exec(function (err, cours) {
+        if (err) {
+            res.send(err)
+        }
+        else {
+            res.send(cours)
+        }
+    })
+})
+
+
+router.get('/deleteCours/:id', function (req, res, next) {
+    var id = req.params.id
+
+    Cours.findByIdAndRemove(id).exec(function (err, cours) {
+        if (err) {
+            res.send(err)
+        }
+        else {
+            res.send(cours)
+        }
+    })
+})
+
+router.post('/updateCours/:id', function (req, res, next) {
+    console.log(req.body)
+    var id = req.params.id
+    Cours.findByIdAndUpdate({ "_id": id }, { $set: { titre: req.body.titre, contenue: req.body.contenue, type: req.body.type, image: req.body.image, date: new Date(), number: req.body.number} }).exec(function (err, cours) {
+        if (err) {
+            res.send(err)
+        }
+        else {
+            res.send(cours)
+        }
+    })
+})
+module.exports = router;
---
+++ api/apiUser.js
@@ -0,0 +1,213 @@
+var express = require('express')
+var bcrypt = require('bcrypt')
+var jwt = require('jsonwebtoken')
+var User = require('../model/users')
+var Candidats = require('../model/candidats')
+
+
+
+var router = express.Router()
+
+
+
+const JWT_SIGN_SECRET = 'KJN4511qkqhxq5585x5s85f8f2x8ww8w55x8s52q5w2q2'
+
+
+router.post('/registerCandidat', function (req, res) {
+    Candidats.findOne({
+            email: req.body.email
+        })
+        .then(function (userfound) {
+            if (!userfound) {
+                bcrypt.hash(req.body.password, 10, function (err, bcryptedPassword) {
+                    var newCandidat = new Candidats({
+                        nom: req.body.nom,
+                        prenom: req.body.prenom,
+                        tel: req.body.tel,
+                        age: req.body.age,
+                        niveau: req.body.niveau,
+                        etat: req.body.etat,
+                        status: req.body.status
+                    });
+                    newCandidat.save().then(function () {
+                            Candidats.findById(newCandidat._id).exec(function (err, cand) {
+                                console.log(cand)
+                                var newUser = new User({
+                                    email: req.body.email,
+                                    candidat: cand._id,
+                                    password: bcryptedPassword,
+                                });
+                                newUser.save().then(function (newUser) {
+                                    res.status(201).send({
+                                        'message': "Candidat ajoutée",
+                                        '_id': newUser._id
+                                    })
+                                    console.log("ok");
+                                })
+                            })
+                        })
+                        .catch(function (err) {
+                            res.status(500).send(err)
+                        })
+                })
+            } else {
+                res.status(409).send({
+                    'error': 'user already exsit'
+                })
+            }
+        })
+        .catch(function (err) {
+            res.status(500).send({
+                'error': 'unable to verify user'
+            })
+        });
+})
+
+
+router.get('/getuser/:id', function (req, res, next) {
+    var id = req.params.id
+    Candidats.findById(id).exec(function (err, user) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(user)
+        }
+    })
+})
+
+router.post('/login', function (req, res) {
+    var password = req.body.password
+    var email = req.body.email
+    if (password == null || email == null) {
+        res.status(400).send({
+            'error': 'missing parametres'
+        })
+    }
+    Candidats.findOne({
+        "email": email
+    }).exec(function (err, userfound) {
+        if (userfound) {
+            bcrypt.compare(password, userfound.password, function (err, resBycrypt) {
+                if (resBycrypt) {
+
+                    const token = jwt.sign({
+                            '_id': userfound._id,
+                            'email': userfound.email,
+                            'nom': userfound.nom,
+                            'prenom': userfound.prenom,
+                            'age': userfound.age,
+                            'tel': userfound.tel,
+                            'niveau': userfound.niveau,
+                            'etat': userfound.etat,
+                            'status': userfound.status,
+                        },
+                        JWT_SIGN_SECRET, {
+                            expiresIn: '1h'
+                        });
+                    res.status(200).send({
+                        Message: 'authentification valide',
+                        token: token
+                    })
+
+                } else {
+                    res.status(403).send({
+                        'error': 'invalid password'
+                    })
+                }
+
+            });
+        } else {
+            res.status(404).send({
+                'error': 'user not exist in DB'
+            })
+        }
+
+    })
+
+});
+
+router.get('/logout', function (req, res) {
+    req.logOut()
+    req.session.destroy()
+
+})
+
+router.post('/addCandidats/:id', function (req, res, next) {
+
+    Candidats.updateOne({
+        "_id": req.params.id
+    }, {
+        $set: {
+            nom: req.body.nom,
+            prenom: req.body.prenom,
+            email: req.body.email,
+            password: req.body.password,
+            age: req.body.age,
+            tel: req.body.tel,
+            niveau: req.body.niveau,
+            status: req.body.status,
+            etat: req.body.etat,
+        }
+    }).exec(function (err, user) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(user)
+        }
+    })
+})
+
+
+
+router.post('/updateUser/:id', function (req, res, next) {
+
+    var id = req.params.id
+
+
+    Candidats.findByIdAndUpdate({
+        "_id": id
+    }, {
+        $set: {
+            nom: req.body.nom,
+            prenom: req.body.prenom,
+            email: req.body.email,
+            tel: req.body.tel,
+            age: req.body.age,
+            niveau: req.body.niveau,
+            status: req.body.status,
+            etat: req.body.etat,
+        }
+    }).exec(function (err, user) {
+        if (err) {
+            res.send(err)
+
+        } else {
+
+
+            User.findById(id).exec(function (err, user) {
+
+                const token = jwt.sign({
+                        _id: user._id,
+                        email: user.email,
+                        nom: user.nom,
+                        prenom: user.prenom,
+                        age: user.age,
+                        etat: req.body.etat,
+                        tel: req.body.tel,
+                        niveau: req.body.niveau,
+                        status: req.body.status,
+
+                    },
+                    JWT_SIGN_SECRET, {
+                        expiresIn: '1h'
+                    });
+                res.status(200).send({
+                    Message: 'Update token ',
+                    token: token,
+                })
+            })
+        }
+    })
+})
+
+module.exports = router;
